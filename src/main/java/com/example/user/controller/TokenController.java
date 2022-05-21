8d5f730a4337e5ccb2df5a0c39c90a020dddba03
Elliot Jee <elliot00jee@gmail.com>
2022-05-21 23:13:14 +0900

    Refactoring - 메소드명 변경: getTokens -> getTokensByOneTimeCode

--- src/main/java/com/example/user/controller/TokenController.java
+++ src/main/java/com/example/user/controller/TokenController.java
@@ -4,16 +4,12 @@ package com.example.user.controller;
 import com.example.user.controller.dto.AuthDto;
 import com.example.user.entity.Tokens;
 import com.example.user.service.TokenService;
-import com.example.user.util.ResponseUtils;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletResponse;
 
 import static com.example.user.util.ResponseUtils.addRefreshTokenCookie;
 import static com.example.user.util.ResponseUtils.success;
@@ -25,8 +21,8 @@ public class TokenController {
     private final TokenService tokenService;
 
     @GetMapping("/tokens/{code}")
-    public ResponseEntity<?> getTokens(@PathVariable String code, HttpServletResponse response) {
-        Tokens tokens = tokenService.getTokens(code);
+    public ResponseEntity<?> getTokens(@PathVariable String code) {
+        Tokens tokens = tokenService.getTokensByOneTimeCode(code);
 
         AuthDto authDto = new AuthDto();
         authDto.setAccesstoken(tokens.getAccessToken());
