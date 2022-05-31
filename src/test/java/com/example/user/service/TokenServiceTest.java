b8ad0c8dda730b795e8cc35df9d01ecbc899fc9d
elliot00jee <elliot00jee@gmail.com>
2022-05-31 10:29:00 +0900

    Test - 토큰 생성 단위 테스트 generateTokens

--- src/test/java/com/example/user/service/TokenServiceTest.java
+++ src/test/java/com/example/user/service/TokenServiceTest.java
@@ -5,17 +5,14 @@ import com.example.user.entity.UserProfiles;
 import com.example.user.security.AbstractJwtService;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.jupiter.params.shadow.com.univocity.parsers.annotations.Nested;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
-
-import java.util.Date;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.mockito.ArgumentMatchers.*;
@@ -27,42 +24,66 @@ import static org.mockito.Mockito.*;
 class TokenServiceTest {
     @Mock
     RedisService redisService;
-
     @Mock
     AbstractJwtService jwtService;
+    static UserProfiles userProfiles;
+    static ObjectMapper objectMapper;
 
-    @Autowired
-    ObjectMapper objectMapper;
-
-    @Nested
-    @Test
-    void generateTokens_needOneTimeCode_true_verify_Tokens() throws JsonProcessingException {
-        UserProfiles userProfiles = UserProfiles.builder()
+    @BeforeAll
+    static void createUserProfiles() {
+        userProfiles = UserProfiles.builder()
                 .username("Elliot Jee")
                 .department("Software engineering")
                 .userId("elliot.jee")
                 .role("Admin")
                 .build();
-
-        doNothing().when(redisService).update(anyString(), anyString(), anyLong());
-
-        when(jwtService.generateToken(any(), any(), any())).thenReturn("accesstoken");
-        when(jwtService.generateToken(eq(null), eq(null), any())).thenReturn("refreshtoken");
-
-        TokenService tokenService = new TokenService(jwtService, redisService, objectMapper);
-
-        Tokens tokens = tokenService.generateTokens(userProfiles, true);
-
-        assertThat(tokens.getJwtId()).isNotNull();
-        assertThat(tokens.getUserId()).isEqualTo(userProfiles.getUserId());
-        assertThat(tokens.getAccessToken()).isEqualTo("accesstoken");
-        assertThat(tokens.getRefreshToken()).isEqualTo("refreshtoken");
-        assertThat(tokens.isOneTimeCodeValid()).isTrue();
-
-//        verify(redisService, times(1))`
-//                .update(any(), eq(objectMapper.writeValueAsString(tokens)), eq(5 * 60L));
+        objectMapper = new ObjectMapper();
     }
 
+    @Nested
+    class generateTokens {
+
+        @Test
+        void needOneTimeCode_true() throws JsonProcessingException {
+            doNothing().when(redisService).update(anyString(), anyString(), anyLong());
+
+            when(jwtService.generateToken(any(), any(), any())).thenReturn("accesstoken");
+            when(jwtService.generateToken(eq(null), eq(null), any())).thenReturn("refreshtoken");
+
+            TokenService tokenService = new TokenService(jwtService, redisService, objectMapper);
+
+            Tokens tokens = tokenService.generateTokens(userProfiles, true);
+
+            assertThat(tokens.getJwtId()).isNotNull();
+            assertThat(tokens.getUserId()).isEqualTo(userProfiles.getUserId());
+            assertThat(tokens.getAccessToken()).isEqualTo("accesstoken");
+            assertThat(tokens.getRefreshToken()).isEqualTo("refreshtoken");
+            assertThat(tokens.isOneTimeCodeValid()).isTrue();
+
+            verify(redisService, times(1))
+                .update(any(), eq(objectMapper.writeValueAsString(tokens)), eq(5 * 60L));
+        }
+        @Test
+        void needOneTimeCode_false() throws JsonProcessingException {
+            doNothing().when(redisService).update(anyString(), anyString(), anyLong());
+
+            when(jwtService.generateToken(any(), any(), any())).thenReturn("accesstoken");
+            when(jwtService.generateToken(eq(null), eq(null), any())).thenReturn("refreshtoken");
+
+            TokenService tokenService = new TokenService(jwtService, redisService, objectMapper);
+
+            Tokens tokens = tokenService.generateTokens(userProfiles, false);
+
+            assertThat(tokens.getJwtId()).isNotNull();
+            assertThat(tokens.getUserId()).isEqualTo(userProfiles.getUserId());
+            assertThat(tokens.getAccessToken()).isEqualTo("accesstoken");
+            assertThat(tokens.getRefreshToken()).isEqualTo("refreshtoken");
+            assertThat(tokens.isOneTimeCodeValid()).isFalse();
+
+            verify(redisService, times(1))
+                    .update(any(), eq(objectMapper.writeValueAsString(tokens)), eq(24 * 60 * 60L));
+        }
+    }
 
 
 }
